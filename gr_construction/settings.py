7c490f1a245e54087e4304d0189700e8f819ed68
IRB <iureyes@icloud.com>
2024-04-16 22:21:17 -0600

    Sprint 02
    First Creation of Project Manager Model

--- gr_construction/settings.py
+++ gr_construction/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gr_construction.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
